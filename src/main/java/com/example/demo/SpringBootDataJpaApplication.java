3efa93feca1fb72aa883d7385600106c5de3064b
unknown <shivakumardata@gmail.com>
2021-02-09 18:28:30 +0530

    few classes have been updated

--- src/main/java/com/example/demo/SpringBootDataJpaApplication.java
+++ src/main/java/com/example/demo/SpringBootDataJpaApplication.java
@@ -2,12 +2,14 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SpringBootDataJpaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootDataJpaApplication.class, args);
+		
+		ConfigurableApplicationContext run = SpringApplication.run(SpringBootDataJpaApplication.class, args);
 	}
 
 }
